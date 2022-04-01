4cff5637594664f677390e606eae9e81a9d8af3b
zakyasra <92093107+zakyasra@users.noreply.github.com>
2022-04-01 21:18:12 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,184 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: "15.000.000",
+    desc: "Ini sepatu.",
+    link: "air-force.html",
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: "10.000.000",
+    desc: "Ini juga sepatu.",
+    link: "jordan.html",
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: "5.000.000",
+    desc: "Ini juga sepatu cok.",
+    link: "blazer.html",
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: "7.000.000",
+    desc: "Lah ini sepatu.",
+    link: "crater.html",
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: "3.000.000",
+    desc: "Sepatu lagi njir.",
+    link: "hippie.html",
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductDesc = document.querySelector(".productDesc");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+const currentProdukButton = document.querySelector(".produkButton");
+
+
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+    currentProdukButton.href = choosenProduct.link;
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "Rp" + choosenProduct.price;
+    currentProductDesc.textContent = choosenProduct.desc;
+    currentProductImg.src = choosenProduct.colors[0].img; 
+
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
+
+const login = document.querySelector(".login");
+const loginForm = document.querySelector(".login-form");
+const closeForm = document.querySelector(".closeLogin");
+
+login.addEventListener("click", () => {
+  loginForm.style.display = "flex"
+})
+
+closeForm.addEventListener("click", () => {
+  loginForm.style.display = "none";
+});
+
+const user = document.querySelector(".usernameInput");
+const userPass = document.querySelector(".passwordInput");
+const logCheck = document.querySelector(".loginButton");
+
+function setUser(username) {
+  login.innerText =  `Hai, ${username}`
+  
+}
+
+logCheck.addEventListener("click", () => {
+  if (user.value != '' && userPass.value != '') {
+    setUser(user.value);
+    loginForm.style.display = "none";  
+  } else {
+    alert("Masukkan data dengan benar!")
+  }
+})
